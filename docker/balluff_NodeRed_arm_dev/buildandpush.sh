ae956fdb8fa453a45b6937ac80718109774073df
Philipp Echteler <philipp.echteler@balluff.de>
2019-08-06 10:43:01 +0200

    some changes

--- docker/balluff_NodeRed_arm_dev/buildandpush.sh
+++ docker/balluff_NodeRed_arm_dev/buildandpush.sh
@@ -8,4 +8,4 @@ TAG="balluff_arm_dev"
 
 docker rmi ${REGISTRY}/${IMAGE_NAME}:${TAG}
 docker build -t ${REGISTRY}/${IMAGE_NAME}:${TAG} --rm .
-docker push ${REGISTRY}/${IMAGE_NAME}:${TAG}
+#docker push ${REGISTRY}/${IMAGE_NAME}:${TAG}
